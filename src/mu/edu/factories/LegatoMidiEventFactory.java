c54a732a249008bf4be955cc749ef403212d92bf
MichaelToast <michaelp86788@gmail.com>
2025-03-31 20:34:30 -0500

    Finished specific EventFactory method definitions

--- src/mu/edu/factories/LegatoMidiEventFactory.java
+++ src/mu/edu/factories/LegatoMidiEventFactory.java
@@ -1,24 +1,38 @@
 package mu.edu.factories;
 
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class LegatoMidiEventFactory implements MidiEventFactory{
 
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub 
-		/* Called with: track.add(factory.createNoteOn())
-		 * track is a sequence
-		 * */
+		ShortMessage mess = null;
+		try {
+			mess = new ShortMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		} catch (javax.sound.midi.InvalidMidiDataException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		MidiEvent midi = new MidiEvent(mess, tick);
 		
-		
-		return null;
+		return midi;
 	}
-
+	
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		// Increased duration of 80
+		ShortMessage mess = null;
+		try {
+			mess = new ShortMessage(ShortMessage.NOTE_OFF, channel, note);
+		} catch (javax.sound.midi.InvalidMidiDataException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		}
+		MidiEvent midi = new MidiEvent(mess, tick + 80);
+		
+		return midi;
 	}
     
 }
--- src/mu/edu/factories/StaccatoMidiEventFactory.java
+++ src/mu/edu/factories/StaccatoMidiEventFactory.java
@@ -1,19 +1,38 @@
 package mu.edu.factories;
 
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class StaccatoMidiEventFactory implements MidiEventFactory {
 
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		ShortMessage mess = null;
+		try {
+			mess = new ShortMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		} catch (javax.sound.midi.InvalidMidiDataException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		MidiEvent midi = new MidiEvent(mess, tick);
+		
+		return midi;
 	}
 
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		// Increased duration of 120
+		ShortMessage mess = null;
+		try {
+			mess = new ShortMessage(ShortMessage.NOTE_OFF, channel, note);
+		} catch (javax.sound.midi.InvalidMidiDataException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		}
+		MidiEvent midi = new MidiEvent(mess, tick + 120);
+		
+		return midi;
 	}
     
 }
