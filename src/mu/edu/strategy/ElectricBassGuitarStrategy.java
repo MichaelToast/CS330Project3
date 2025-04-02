ab2146802c7ee0068ac00e493587469fac91e293
noahmcallister04 <116388091+noahmcallister04@users.noreply.github.com>
2025-04-02 12:13:36 -0500

    Implemented BassGuitarStrategy

--- src/mu/edu/strategy/ElectricBassGuitarStrategy.java
+++ src/mu/edu/strategy/ElectricBassGuitarStrategy.java
@@ -3,8 +3,21 @@ package mu.edu.strategy;
 import javax.sound.midi.Track;
 
 public class ElectricBassGuitarStrategy implements InstrumentStrategy{
-	public void applyInstrument(Track track, int channel) {
-		// TODO Auto-generated method stub
-		
-	}
+	private static final int ELECTRIC_BASS_FINGER = 33;
+    
+    @Override
+    public void applyInstrument(Track track, int channel) {
+        try {
+
+            ShortMessage instrumentChange = new ShortMessage();
+            instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, ELECTRIC_BASS_FINGER, 0);
+            
+
+            track.add(new MidiEvent(instrumentChange, 0));
+            
+            System.out.println("Applied Electric Bass Guitar (finger) to channel " + channel);
+        } catch (InvalidMidiDataException e) {
+            System.err.println("Error applying Electric Bass Guitar: " + e.getMessage());
+        }
+    }
 }
