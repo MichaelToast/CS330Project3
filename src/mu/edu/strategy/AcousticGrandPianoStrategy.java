51462b9f6ca36f5330da0f940cd8a68810cd11d1
noahmcallister04 <116388091+noahmcallister04@users.noreply.github.com>
2025-04-02 12:12:14 -0500

    Implemented GrandPianoStrategy

--- src/mu/edu/strategy/AcousticGrandPianoStrategy.java
+++ src/mu/edu/strategy/AcousticGrandPianoStrategy.java
@@ -3,9 +3,21 @@ package mu.edu.strategy;
 import javax.sound.midi.Track;
 
 public class AcousticGrandPianoStrategy implements InstrumentStrategy{
-	public void applyInstrument(Track track, int channel) {
-		// TODO Auto-generated method stub
-		
-	}
+	private static final int ACOUSTIC_GRAND_PIANO = 0;
     
+    @Override
+    public void applyInstrument(Track track, int channel) {
+        try {
+
+            ShortMessage instrumentChange = new ShortMessage();
+            instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, ACOUSTIC_GRAND_PIANO, 0);
+            
+
+            track.add(new MidiEvent(instrumentChange, 0));
+            
+            System.out.println("Applied Acoustic Grand Piano to channel " + channel);
+        } catch (InvalidMidiDataException e) {
+            System.err.println("Error applying Acoustic Grand Piano: " + e.getMessage());
+        }
+    }
 }
