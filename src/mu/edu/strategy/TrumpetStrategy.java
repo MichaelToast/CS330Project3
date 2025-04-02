6aa9777acb8cea65b25b7138e3697b9e9215813c
noahmcallister04 <116388091+noahmcallister04@users.noreply.github.com>
2025-04-02 12:14:30 -0500

    Implemented TrumpetStrategy

--- src/mu/edu/strategy/TrumpetStrategy.java
+++ src/mu/edu/strategy/TrumpetStrategy.java
@@ -3,8 +3,21 @@ package mu.edu.strategy;
 import javax.sound.midi.Track;
 
 public class TrumpetStrategy implements InstrumentStrategy{
-	public void applyInstrument(Track track, int channel) {
-		// TODO Auto-generated method stub
-		
-	}
+	private static final int TRUMPET = 56;
+    
+    @Override
+    public void applyInstrument(Track track, int channel) {
+        try {
+
+            ShortMessage instrumentChange = new ShortMessage();
+            instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, TRUMPET, 0);
+            
+
+            track.add(new MidiEvent(instrumentChange, 0));
+            
+            System.out.println("Applied Trumpet to channel " + channel);
+        } catch (InvalidMidiDataException e) {
+            System.err.println("Error applying Trumpet: " + e.getMessage());
+        }
+    }
 }
